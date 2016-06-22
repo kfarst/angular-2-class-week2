35b409ef5453d37e7eb3425b54c2a85c73d23937
Kevin Farst <kevin.farst@zozi.com>
2016-06-22 01:16:10 -0700

    Running Angular 1 and 2 in parallel

---
+++ src/systemjs.config.js
@@ -0,0 +1,55 @@
+System.config({
+    map: {
+        'rxjs': 'rxjs',
+        '@angular': '@angular',
+        'app': './'
+    },
+    packages: {
+        'app': {
+            main: '.js',
+            defaultExtension: 'js'
+        },
+        '@angular/core': {
+            main: 'index.js',
+            defaultExtension: 'js'
+        },
+        '@angular/http': {
+            main: 'index.js',
+            defaultExtension: 'js'
+        },
+        '@angular/compiler': {
+            main: 'index.js',
+            defaultExtension: 'js'
+        },
+        '@angular/router-deprecated': {
+            main: 'index.js',
+            defaultExtension: 'js'
+        },
+
+        '@angular/upgrade': {
+            main: 'index.js',
+            defaultExtension: 'js'
+        },
+
+        '@angular/forms': {
+            main: 'index.js',
+            defaultExtension: 'js'
+        },
+
+        '@angular/common': {
+            main: 'index.js',
+            defaultExtension: 'js'
+        },
+        '@angular/platform-browser': {
+            main: 'index.js',
+            defaultExtension: 'js'
+        },
+        '@angular/platform-browser-dynamic': {
+            main: 'index.js',
+            defaultExtension: 'js'
+        },
+        'rxjs': {
+            defaultExtension: 'js'
+        }
+    }
+})
